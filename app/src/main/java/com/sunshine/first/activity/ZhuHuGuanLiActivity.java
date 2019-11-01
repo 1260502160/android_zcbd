b08e648a3664cb90a172dd1cd6823781b81c0fea
裴杨帆 <peiclia@com.com>
2019-11-02 00:04:30 +0800

    首页滑动

--- app/src/main/java/com/sunshine/first/activity/ZhuHuGuanLiActivity.java
+++ app/src/main/java/com/sunshine/first/activity/ZhuHuGuanLiActivity.java
@@ -4,13 +4,11 @@ import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import com.sunshine.first.BaseAppCompatActivity;
 import com.sunshine.first.R;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -46,7 +44,7 @@ public class ZhuHuGuanLiActivity extends BaseAppCompatActivity {
         Intent intent;
         switch (view.getId()) {
             case R.id.btn_add_household_info:
-                intent = new Intent(ZhuHuGuanLiActivity.this, AddHoueseHoldActivity.class);
+                intent = new Intent(ZhuHuGuanLiActivity.this, HostmanRenActivity.class);
                 startActivity(intent);
                 break;
         }
